3b8bdf6d723618260c6b77b42edf0ecdaa951897
Justin Chiu <justinchiu@g.harvard.edu>
2023-03-14 21:49:16 -0400

    medium

--- code/color.py
+++ code/color.py
@@ -1,12 +1,14 @@
 # dots are x, y, size, color
- 
-def is_black(dot):
-    return dot[-1] > 0.4
 
+# -1 is darkest
+def is_dark(dot):
+    return dot[-1] < -0.4
+
+# 1 is lightest
 def is_light(dot):
     # colors are in [-1,1]
-    return dot[-1] < -0.4
+    return dot[-1] > 0.4
 
 def is_grey(dot):
     return True
-    return not is_black(dot) and not is_light(dot)
+    return not is_dark(dot) and not is_light(dot)
--- code/size.py
+++ code/size.py
@@ -1,11 +1,18 @@
-def is_large():
-    pass
+import numpy as np
 
-def is_small():
-    pass
 
-def largest():
-    pass
+def is_large(dot):
+    return dot[-2] > 0.4
 
-def smallest():
-    pass
+def is_small(dot):
+    return dot[-2] < -0.4
+
+def is_medium(dot):
+    return True
+
+
+def largest(dots):
+    return dots[np.argmax(dots[:,-2])]
+
+def smallest(dots):
+    return dots[np.argmin(dots[:,-2])]
