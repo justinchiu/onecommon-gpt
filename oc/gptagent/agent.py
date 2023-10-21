b64614b9b64c5b88dba542ac51d78edeba2fba00
Justin Chiu <justinchiu@g.harvard.edu>
2023-10-21 13:58:54 -0400

    use tenacity.retry for gptagent completion

--- oc/gptagent/agent.py
+++ oc/gptagent/agent.py
@@ -4,10 +4,24 @@ from pathlib import Path
 import re
 import openai
 import ast
+import string
+import tenacity
 
 from oc.gptagent.construct_prompt import BOARDS, OC_INSTRUCTIONS
 from oc.gptagent.construct_prompt import verbalize_board, construct_prompt
 
+
+@tenacity.retry#(wait=tenacity.wait_fixed(0.5))
+def complete(model, system, user):
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=[
+            {"role": "system", "content": system},
+            {"role": "user", "content": user},
+        ],
+        stop="\n",
+    )
+    return response
 
 class Agent:
     def __init__(self, backend, refres, gen, model="gpt-3.5-turbo"):
@@ -42,35 +56,32 @@ class Agent:
     def write(self):
         prompt_prefix = construct_prompt(self.board_desc, self.turns, self.agent_id)
 
-        response = openai.ChatCompletion.create(
-            model=self.model,
-            messages=[
-                {"role": "system", "content": OC_INSTRUCTIONS},
-                {"role": "user", "content": prompt_prefix},
-            ],
-            stop="\n",
-        )
+        response = complete(self.model, OC_INSTRUCTIONS, prompt_prefix)
         utterance = response.choices[0].message.content
         self.turns.append(utterance)
         split_utterance = utterance.split()
         if split_utterance[1] == "Select":
-            selected_id = split_utterance[2]
-            self.selected_dot = list(map(lambda x: x["id"], self.board)).index(selected_id)
+            selected_id = split_utterance[2].translate(str.maketrans('', '', string.punctuation))
+            string_ids = list(map(lambda x: x["id"], self.board))
+            self.selected_dot = (
+                string_ids.index(selected_id)
+                if selected_id in string_ids
+                else 0
+            )
             split_utterance = ["<selection>"]
         return split_utterance
 
     def choose(self):
         if self.selected_dot is None:
             prompt_prefix = construct_prompt(self.board_desc, self.turns, self.agent_id)
-            response = openai.ChatCompletion.create(
-                model=self.model,
-                messages=[
-                    {"role": "system", "content": OC_INSTRUCTIONS},
-                    {"role": "user", "content": prompt_prefix + "\nYou: Select"},
-                ],
-                stop="\n",
-            )
+            response = complete(self.model, OC_INSTRUCTIONS, prompt_prefix + "\nYou: Select")
             split_utterance = response.choices[0].message.content.split()
-            selected_id = split_utterance[0]
-            self.selected_dot = list(map(lambda x: x["id"], self.board)).index(selected_id)
+            selected_id = split_utterance[0].translate(str.maketrans('', '', string.punctuation))
+            string_ids = list(map(lambda x: x["id"], self.board))
+            self.selected_dot = (
+                string_ids.index(selected_id)
+                if selected_id in string_ids
+                else 0
+            )
+
         return self.selected_dot
